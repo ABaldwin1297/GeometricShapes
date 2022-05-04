a7d0065ac284a32b0bcb37ded87fde90983a8baa
ABaldwin1297 <abaldwin1297@gmail.com>
2022-05-04 15:18:59 -0400

    Add project files.

---
+++ GeometricShapes/Program.cs
@@ -0,0 +1,13 @@
+﻿using GeometricShapes;
+
+var q1 = new Quad() { Side1 = 3, Side2 = 4, Side3 = 5, Side4 = 6 };
+
+Console.WriteLine(q1.Perimeter());
+
+var r1 = new Rect() { Side1 = 3, Side2 = 4 };
+
+Console.WriteLine($"{r1.Perimeter()} | {r1.area()}");
+
+var s1 = new Square() { side1 = 5};
+
+Console.WriteLine($"{s1.Perimeter()} | {s1.area()}");
---
+++ GeometricShapes/Square.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricShapes {
+    internal class Square {
+
+        public int side1 { get; set; } = 0;
+
+        public int Perimeter() {
+            return side1 * 4;
+        }
+        public int area() {
+            return side1 * side1;
+        }
+    }
+}
